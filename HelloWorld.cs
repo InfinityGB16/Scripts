b9df4b7d7da62c6e3d1f2a5a0dcfccb571510dc7
InfinityGB16 <mateogaray311@gmail.com>
2024-12-13 19:18:01 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Le dice hola a Mateo
+        print("Hello, Mateo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
